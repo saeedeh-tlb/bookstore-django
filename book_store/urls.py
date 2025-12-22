c396d52b9f2f6cd92a46770762c92b26304747be
saeedeh <tlb.saeedehh@gmail.com>
2025-12-22 21:55:28 +0330

    first function in views

--- book_store/urls.py
+++ book_store/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from book.views import current_datetime
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('book/', current_datetime),
+
 ]

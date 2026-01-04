15b1862c6e78000f972fd883ba7b9787a6ab1367
saeedeh <tlb.saeedehh@gmail.com>
2026-01-04 20:36:49 +0330

    fourth session part one

--- book/forms.py
+++ book/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from book.models import Book
 
 
-class CreateBook(forms.Form):
+class CreateBook(forms.ModelForm):
     class Meta:
         model = Book
         fields = "__all__"
--- book/views.py
+++ book/views.py
@@ -8,15 +8,20 @@ from datetime import datetime
 from .models import Book
 from book.forms import  CreateBook
 import json
-
+from rest_framework.views import APIView
+from book.serializers import BookSerializer
 
 def get_all_books(request):
     books=list(Book.objects.values())
 
     return JsonResponse(books , safe=False)
 
+def index(request):
+    books=Book.objects.all()
+    return render(request , "books.html" , {"books": books})
+
 @csrf_exempt
-def create_book(request):
+def book(request):
     if request.method == "POST":
         print(request.POST)
         body=json.loads(request.body.decode('utf-8'))
@@ -27,6 +32,22 @@ def create_book(request):
             return JsonResponse({'book_id': book.id})
         return JsonResponse({'error': 'data format is not correct'})
 
-def index(request):
-    books=Book.objects.all()
-    return render(request , "books.html" , {"books": books})
+
+class BookAPI(APIView):
+
+    def post(self , request):
+        body = json.loads(request.body.decode('utf-8'))
+        body['published_date'] = datetime.now()
+        form = BookSerializer(data=body)
+        if form.is_valid():
+            book = form.save()
+            return JsonResponse({'book_id': book.id})
+        return JsonResponse({'error': 'data format is not correct'})
+
+    def get(self , request):
+        books = list(Book.objects.values())
+        return JsonResponse(books, safe=False)
+
+    def delete(self , request):
+        
+
